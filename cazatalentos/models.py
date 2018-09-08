a8be1c419b16ebc9cf06ede110ed363357cdc88b
Yojan Antonio Pardo Zabala <yojan.pardo@gmail.com>
2018-09-08 16:42:05 -0500

    termino configuración de archivo setting.py y agrego los otros modelos

---
+++ cazatalentos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Scout(models.Model):
+    KINDS=(
+        ('A','Cédula de ciudadanía'),
+        ('B','Cédula de extrangería'),
+        ('C','Nit'),
+    )
+    doc_kind = models.CharField(max_length=1,choices=KINDS,default='A')
+    doc = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    second_name = models.Charfield(max_length=20)
+    first_lastname = models.CharField(max_length=20)
+    second_lastname = models.CharField(max_length=20)
+    cellphone_number = models.Charfield(max_length=20)
--- scout_sport/settings.py
+++ scout_sport/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'scout_sport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'scout_sport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scout_sport',
+        'USER': 'scout_sport',
+        'PASSWORD', '123456',
+        'HOST', '127.0.0.1',
+        'PORT', '5432',
     }
 }
 
@@ -118,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',    
+]
